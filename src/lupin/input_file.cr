655d58958afe5320507bc7500c548b317df828d3
Mark Molnar <molnarpw@gmail.com>
2018-09-08 17:36:39 +0200

    InputFile class added

---
+++ src/lupin/input_file.cr
@@ -0,0 +1,12 @@
+module Lupin
+  class InputFile
+    name : String
+    path : String
+    contents : String
+
+    getter name, path, contents
+    setter name, path, contents
+
+    def initialize(@name, @path, @contents)
+  end
+end
